a5867023941da38367e22b0795e88ebec99fc3fc
jamiukabir22 <kabirm474@gmail.com>
2022-10-04 09:03:43 -0700

    header

---
+++ monty.h
@@ -0,0 +1,18 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+extern int global_variable;
+
+/**
+ * function declaration
+ */
+extern void ust_it(void);
+extern int increment(void);
+
+extern void dec(void);
+extern void put(void);
+extern void inc(void);
+
+int some_var;
+
+#endif
